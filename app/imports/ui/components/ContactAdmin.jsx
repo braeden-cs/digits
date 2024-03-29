cad5e1d437f626c452187afd2a06f9c4727a274e
braeden-cs <156963237+braeden-cs@users.noreply.github.com>
2024-03-28 21:25:40 -1000

    contacts collection finished in 40+ minutes.

--- app/imports/ui/components/ContactAdmin.jsx
+++ app/imports/ui/components/ContactAdmin.jsx
@@ -12,6 +12,7 @@ const Contact = ({ contact }) => (
     </Card.Header>
     <Card.Body>
       <Card.Text>{contact.description}</Card.Text>
+      <footer className="blockquote-footer">{contact.owner}</footer>
     </Card.Body>
   </Card>
 );
