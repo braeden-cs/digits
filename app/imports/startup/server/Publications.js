85d023a99773f8f8e407ad9ce2513b2592666a82
braeden-cs <156963237+braeden-cs@users.noreply.github.com>
2024-04-01 19:57:46 -1000

    notes-1 finished in 39 minutes

--- app/imports/startup/server/Publications.js
+++ app/imports/startup/server/Publications.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { Roles } from 'meteor/alanning:roles';
 import { Contacts } from '../../api/contacts/Contacts';
+import { Notes } from '../../api/note/Notes';
 
 // User-level publication.
 // If logged in, then publish documents owned by this user. Otherwise, publish nothing.
@@ -17,6 +18,30 @@ Meteor.publish(Contacts.userPublicationName, function () {
 Meteor.publish(Contacts.adminPublicationName, function () {
   if (this.userId && Roles.userIsInRole(this.userId, 'admin')) {
     return Contacts.collection.find();
+  }
+  return this.ready();
+});
+
+// alanning:roles publication
+// Recommended code to publish roles for each user.
+Meteor.publish(null, function () {
+  if (this.userId) {
+    return Meteor.roleAssignment.find({ 'user._id': this.userId });
+  }
+  return this.ready();
+});
+
+Meteor.publish(Notes.userPublicationName, function () {
+  if (this.userId) {
+    const username = Meteor.users.findOne(this.userId).username;
+    return Notes.collection.find({ owner: username });
+  }
+  return this.ready();
+});
+
+Meteor.publish(Notes.adminPublicationName, function () {
+  if (this.userId && Roles.userIsInRole(this.userId, 'admin')) {
+    return Notes.collection.find();
   }
   return this.ready();
 });
